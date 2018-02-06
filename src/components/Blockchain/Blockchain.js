2b6037988aded6b45ca3b9cc078b8df0352832fe
Brendon <torre.brendon@gmail.com>
2018-02-06 20:57:37 +0100

    Update blockchain's css to be responsive

--- src/components/Blockchain/Blockchain.js
+++ src/components/Blockchain/Blockchain.js
@@ -14,24 +14,24 @@ class Blockchain extends Component {
   render(){
     return (
       <div className="grid">
-        <div className='datas'>
-          <p className='data'>Data</p>
-          <input className='input form-control' type="text" value="Welcome to Brendon's Blockchain"/>
+        <div className='datas gridData'>
+          <p className='data small-border asc bl'>Data</p>
+          <input className='input form-control small-border br' type="text" value="Welcome to Brendon's Blockchain"/>
+        </div>
+        <div className='previousHash'>
+          <p className='label' id='previousHash'>Previous Hash</p>
+          <p className='hash' id='previous'>0</p>
         </div>
         <div className='hashes'>
-          <div className='previousHash'>
-            <p className='label' id='previousHash'>Previous Hash</p>
-            <p className='hash' id='previous'>0</p>
-          </div>
           <p className='label' id='presentHash'>Hash</p>
-          <p className='hash' id='present'>0650646454dsef6se4fs1f3s51f5sef13s51f5se1fs1f35s5ef1</p>
+          <p className='hash small-border bl br' id='present'>0650646454dsef6se4fs1f3s51f5sef13s51f5se1fs1f35s5ef1</p>
         </div>
         <div className='grid2'>
           <div className='grid3'>
-            <p className='blockOrder h2 title'>Genesis Block</p>
+            <p className='blockOrder h2 title asc'>Genesis Block</p>
             <p className='blockDate'>on Tue, 06 Feb 2018 10:46:36 GMT</p>
           </div>
-          <p className='nounce'>2485</p>
+          <p className='nounce small-border asc'>2485</p>
         </div>
       </div>
     );
