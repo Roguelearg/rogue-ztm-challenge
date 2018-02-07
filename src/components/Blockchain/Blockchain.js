8c7e8193e5cbfcae47ca43d6cb9cd83980458219
Brendon <torre.brendon@gmail.com>
2018-02-07 12:35:24 +0100

    Add button to create new blocks

--- src/components/Blockchain/Blockchain.js
+++ src/components/Blockchain/Blockchain.js
@@ -3,17 +3,29 @@ import './Blockchain.css';
 
 class Blockchain extends Component {
 
-  // constructor() {
-  //   super();
-  // }
+  constructor({id}) {
+    super();
+    this.state={
+      id: id,
+      previousHash: '0',
+      hash: '0'
+
+    }
+  }
   //
   // componentDidMount(){
   //
   // }
 
+  renderNumBlock = () => {
+    const {id} = this.state;
+    return id > 0 ? `Block #${id}` : 'Genesis Block';
+  }
+
   render(){
+    console.log(this.state.id);
     return (
-      <div className="grid">
+      <div className="grid mb4">
         <div className='datas gridData'>
           <p className='data small-border asc bl'>Data</p>
           <input className='input form-control small-border br' type="text" value="Welcome to Brendon's Blockchain"/>
@@ -28,7 +40,7 @@ class Blockchain extends Component {
         </div>
         <div className='grid2'>
           <div className='grid3'>
-            <p className='blockOrder h2 title asc'>Genesis Block</p>
+            <p className='blockOrder h2 title asc'>{this.renderNumBlock()}</p>
             <p className='blockDate'>on Tue, 06 Feb 2018 10:46:36 GMT</p>
           </div>
           <p className='nounce small-border asc'>2485</p>
--- src/containers/Blockchains/Blockchains.js
+++ src/containers/Blockchains/Blockchains.js
@@ -6,16 +6,39 @@ class Blockchains extends Component {
 
   constructor() {
     super();
+    this.state = {
+      number: 1
+    }
   }
 
   componentDidMount(){
 
   }
 
+  createBlockchains = () => {
+    const {number} = this.state;
+    let vue = [];
+    vue.push(<Blockchain key={0}  id={0}/>);
+    for(let i = 1; i < number; i++) {
+      vue.push(<i className="material-icons mb4" key={`icon${i}`}>keyboard_arrow_down</i>);
+      vue.push(<Blockchain key={i} id={i}/>);
+    }
+
+    return vue;
+  }
+
+  addBlock = () => {
+    this.setState({number: this.state.number+1});
+  }
+
   render(){
     return (
       <div className='chain'>
-        <Blockchain />
+        {this.createBlockchains().map(block => {
+          return block;
+        })
+        }
+        <button type="button" className="btn btn-primary" onClick={this.addBlock}>Add Block</button>
       </div>
     );
   }
